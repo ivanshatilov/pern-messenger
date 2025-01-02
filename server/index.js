e8a8555595e047743d579a85c3b994c23a5213fd
Ivan Shatilov <shatilov.ivan2oo1@gmail.com>
2025-01-02 23:37:04 +0600

    feat: (server) added .env file, connected the database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
+require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db.js');
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server started successfully on port: ${PORT}`);
-})
+
+
+const start = async () => {
+    try {
+        sequelize.authenticate();
+        sequelize.sync();
+
+        app.listen(PORT, () => {
+            console.log(`Server started successfully on port: ${PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
+
